56e1fc06577b1acba960d096b87bfe1a5c621842
kitostock <ito0405r.s@gmail.com>
2022-03-12 00:57:53 +0900

    modify setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,10 +29,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ['bop-poker.herokuapp.com']
+ALLOWED_HOSTS = ['bop-porker2.herokuapp.com']
 # localよう
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
